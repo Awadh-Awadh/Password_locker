91a8409bf37831f6408ab14fa6d214447cdcdd6a
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-01 22:10:08 +0300

    add underlines and call the main function

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from account import User
 def main():
     while True:
         print("Welcome to password locker")
+        print('-'*30)
         print('\n')
         print("Select a short codes to navigate: to create a new user use 'nn': To login to your account use: 'lg' To exit use: 'ex'")
         short_code = input().lower()
@@ -68,7 +69,8 @@ def main():
         else:
             print("Enter valid code to continue")
 
-              
+if __name__ == '__main__':
+    main()         
                  
 
 
