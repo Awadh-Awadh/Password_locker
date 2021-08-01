eae554d3dd067cd0431f25385fb6d707a600d850
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-01 22:32:15 +0300

    create a class that define test cases for users

---
+++ user_test.py
@@ -0,0 +1,15 @@
+import unittest
+from account import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def SetUp(self):
+        self.new_account = User("awadh","Dreezy@84")
+    
+    # def test_init(self)
